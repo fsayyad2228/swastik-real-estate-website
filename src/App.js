2858d1e0219627fc39e4aa029efaf529ac4e3d3b
Firoj Sayyad <fsayyad3478@gmail.com>
2024-03-04 17:16:29 +0530

    footer Layout

--- src/App.js
+++ src/App.js
@@ -1,46 +1,43 @@
-import React from 'react'
-import "./Styles/style.css"
-import Footer from './Partials/HeaderFooter/Footer'
-import Navbar from './Partials/NavSection/Navbar'
-import ContactForm from './Utils/ContactForm'
-import HeroSection from './Components/HeroSection'
-import ServiceSection from './Components/AboutSection'
-import BodySection from './Components/BodySection'
-import ProductionSection from './Components/ProductServiceSection'
-import TestimonialSection from './Components/TestimonialSection'
-import ExpertOpenionSection from './Components/ExpertOpenionSection'
-import HomePage from './Components/HomePage'
+import React from "react";
+import "./Styles/style.css";
+import Footer from "./Partials/HeaderFooter/Footer";
+import Navbar from "./Partials/NavSection/Navbar";
+import ContactForm from "./Utils/ContactForm";
+import HeroSection from "./Components/HeroSection";
+import ServiceSection from "./Components/AboutSection";
+import BodySection from "./Components/BodySection";
+import ProductionSection from "./Components/ProductServiceSection";
+import TestimonialSection from "./Components/TestimonialSection";
+import ExpertOpenionSection from "./Components/ExpertOpenionSection";
+import HomePage from "./Components/HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CursorPointer from './Utils/CursorPointer'
+import CursorPointer from "./Utils/CursorPointer";
+
 function App() {
   return (
     <>
-    <CursorPointer/>
-      <div >
+      <CursorPointer />
+      <div>
         <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<HomePage />}></Route>
-          <Route exact path="/hero" element={<HeroSection />}></Route>
-          <Route path="/service" element={<ServiceSection />}></Route>
-          <Route path="/body" element={<BodySection />}></Route>
-          <Route path="/production" element={<ProductionSection />}></Route>
-          <Route path="/testimonial" element={<TestimonialSection />}></Route>
-          <Route path="/expert" element={<ExpertOpenionSection />}></Route>
-          <Route path="/contact" element={<ContactForm />}></Route>
-          <Route path="/footer" element={<Footer />}></Route>
-
-        </Routes>
-      </Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<HomePage />}></Route>
+            <Route exact path="/hero" element={<HeroSection />}></Route>
+            <Route path="/service" element={<ServiceSection />}></Route>
+            <Route path="/body" element={<BodySection />}></Route>
+            <Route path="/production" element={<ProductionSection />}></Route>
+            <Route path="/testimonial" element={<TestimonialSection />}></Route>
+            <Route path="/expert" element={<ExpertOpenionSection />}></Route>
+            <Route path="/contact" element={<ContactForm />}></Route>
+            <Route path="/footer" element={<Footer />}></Route>
+          </Routes>
+        </Router>
       </div>
     </>
   );
 }
 
 export default App;
-
-
-
 
 // import React from 'react'
 // import "./Styles/style.css"
@@ -67,7 +64,6 @@ export default App;
 //         <ExpertOpenionSection />
 //         <ContactForm />
 //         <Footer />
-
 
 //       </div>
 //     </>
